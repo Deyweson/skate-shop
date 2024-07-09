ac8c2583c680d2a458c9f077b4051bdeeb17166a
Deyweson <deywesonalm@gmail.com>
2024-07-09 15:34:24 -0300

    Feat: Criado funcionaldiade para troca dinamica  de imagens do produto

--- produto.js
+++ produto.js
@@ -3,28 +3,59 @@ const nomeProduto = document.querySelector('.nome-produto')
 const descricaoProduto = document.querySelector('.descricao-produto')
 const valorProduto = document.querySelector('.valor-produto')
 const tamanhosProduto = document.querySelector('.tamanhos')
+const btnLeft = document.querySelector('.btn-left')
+const btnRight = document.querySelector('.btn-right')
 
 var queryString = window.location.search.substring(1);
 var queryArray = queryString.split("=");
 var id = queryArray[1]
 
-
+if (id === null || id === undefined) {
+  window.location.href = "index.html";
+}
 
 const preencherProduto = async () => {
   const fetch = await axios.get('./produtos.json')
   const produto = fetch.data.produtos[id - 1]
 
-  
+
 
   nomeProduto.textContent = produto.nome
   descricaoProduto.textContent = produto.desc
   valorProduto.textContent = produto.valor
 
-  for(const tam of produto.tam) {
+  for (const tam of produto.tam) {
     const item = document.createElement('li')
     item.textContent = tam
     tamanhosProduto.appendChild(item)
   }
+
+
+  let index = 0
+  imgProduto.src = produto.galeria[index]
+  const qtdImgs = produto.galeria.length
+  console.log(qtdImgs)
+
+  btnRight.addEventListener('click', () => {
+    if (index < qtdImgs - 1) {
+      index++
+    } else {
+      index = 0
+    }
+    console.log(index<qtdImgs)
+    imgProduto.src = produto.galeria[index]
+  })
+  btnLeft.addEventListener('click', () => {
+    if (index > 0) {
+      index = index - 1
+    } else {
+      index = qtdImgs - 1
+    }
+    console.log(index<qtdImgs, index, qtdImgs)
+    imgProduto.src = produto.galeria[index]
+  })
+
+
 }
 
 preencherProduto()
